e470050ca365bb31f8a2ada1cb4a8380bc33b5ab
Omelchenko_Viktor <omelchenko.cr@gmail.com>
2017-02-22 19:25:11 +0200

    ObjectExtensions

---
+++ PostponedPosting.Common/Extensions/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostponedPosting.Common.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static void ThrowsWhenNull(this object obj, string paramName = null)
+        {
+            if (obj == null)
+            {
+                if (string.IsNullOrEmpty(paramName))
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                {
+                    throw new ArgumentNullException(paramName);
+                }
+            }
+        }
+    }
+}
---
+++ PostponedPosting.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PostponedPosting.WebUI.MvcApplication" Language="C#" %>
